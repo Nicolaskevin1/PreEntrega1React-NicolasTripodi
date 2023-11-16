b0956fecf963ad83bd525063d1443386566fbd5d
Nicolas Tripodi <nico2002online@gmail.com>
2023-11-15 21:53:10 -0300

    Modificaciones

---
+++ preentrega1/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <div className="navbar"> 
+            <h3>Inicio</h3>
+            <h3>Contacto</h3>
+            <h3>Productos</h3>
+            <h3>Nosotros</h3>
+        </div>
+    )
+}
